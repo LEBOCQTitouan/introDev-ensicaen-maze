4e915b183e7a4c6d6a929aa2b08ea03ac35f1734
LEBOCQ Titouan <lebocq.titouan@gmail.com>
2021-09-30 11:11:48 +0200

    creating different maze displays

--- lib/mazeDisplay/mazeDisplay.c
+++ lib/mazeDisplay/mazeDisplay.c
@@ -1,20 +1,59 @@
 #include "mazeDisplay.h"
 
-void displayMaze(maze maze) {
-    for (int i = 0; i < maze.width; i++)
-    {
-        for (int j = 0; j < maze.height; j++)
+void displayMaze(maze maze, mazeDisplayType displayType) {
+    if (displayType == DETAILLED_MAZE_DISPLAY) {
+        printfColored(YELLOW, DEFAULT_COLOR, BOLD, "(%d (width) x %d (height))\n", maze.width, maze.height);
+        for (int i = 0; i < maze.height; i++)
         {
-            if (maze.elements[i][j] == MAZE_ENTRANCE) {
-                printfColored(GREEN, DEFAULT_COLOR, BOLD, "%c", MAZE_ENTRANCE);
-            } else if (maze.elements[i][j] == MAZE_EXIT) {
-                printfColored(RED, DEFAULT_COLOR, BOLD, "%c", MAZE_EXIT);
-            } else if (maze.elements[i][j] == MAZE_CORRIDOR) {
-                printfColored(WHITE, DEFAULT_COLOR, BOLD, "%c", MAZE_CORRIDOR);
-            } else {
-                printf("%c", MAZE_CORRIDOR);
+            if (i == 0) {
+                printf("    ");
+                for (int j = 0; j < maze.width; j++) printf("%d", j % 10);
+                printf("\n");
+                printf("    ");
+                for (int j = 0; j < maze.width; j++) printf("v");
+                printf("\n");
             }
+            printf("%2d> ", i);
+            for (int j = 0; j < maze.width; j++)
+            {
+                if (maze.elements[j][i] == MAZE_ENTRANCE) {
+                    printfColored(GREEN, DEFAULT_COLOR, BOLD, "%c", MAZE_ENTRANCE);
+                } else if (maze.elements[j][i] == MAZE_EXIT) {
+                    printfColored(RED, DEFAULT_COLOR, BOLD, "%c", MAZE_EXIT);
+                } else if (maze.elements[j][i] == MAZE_WALL) {
+                    printfColored(WHITE, DEFAULT_COLOR, BOLD, "%c", MAZE_WALL);
+                } else {
+                    printfColored(DEFAULT_COLOR, YELLOW, DEFAULT_STYLE, "%c", MAZE_CORRIDOR);
+                }
+            }
+            printf("\n");
         }
-        printf("\n");
+    } else if (displayType == MINIMALIST_MAZE_DISPLAY) {
+        for (int i = 0; i < maze.height; i++)
+        {
+            for (int j = 0; j < maze.width; j++)
+            {
+                printf("%c", maze.elements[j][i]);
+            }
+            printf("\n");
+        }
+    } else {
+       for (int i = 0; i < maze.height; i++)
+       {
+           for (int j = 0; j < maze.width; j++)
+            {
+                if (maze.elements[j][i] == MAZE_ENTRANCE) {
+                    printfColored(GREEN, DEFAULT_COLOR, BOLD, "%c", MAZE_ENTRANCE);
+                } else if (maze.elements[j][i] == MAZE_EXIT) {
+                    printfColored(RED, DEFAULT_COLOR, BOLD, "%c", MAZE_EXIT);
+                } else if (maze.elements[j][i] == MAZE_WALL) {
+                    printfColored(WHITE, DEFAULT_COLOR, BOLD, "%c", MAZE_WALL);
+                } else {
+                    printfColored(DEFAULT_COLOR, YELLOW, DEFAULT_STYLE, "%c", MAZE_CORRIDOR);
+                }
+            }
+            printf("\n");
+       }
+       
     }
 }
--- lib/mazeDisplay/mazeDisplay.h
+++ lib/mazeDisplay/mazeDisplay.h
@@ -6,6 +6,12 @@
 #include "../BetterTerminal/betterTerminal.h"
 #include "../maze/maze.h"
 
-void displayMaze(maze maze);
+typedef enum {
+    MINIMALIST_MAZE_DISPLAY,
+    DEFAULT_MAZE_DISPLAY,
+    DETAILLED_MAZE_DISPLAY
+} mazeDisplayType;
+
+void displayMaze(maze maze, mazeDisplayType displayType);
 
 #endif // DISPLAY_H
