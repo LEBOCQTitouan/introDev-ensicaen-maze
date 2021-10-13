2fa6067e8d46b1ffa0201ff44cdbf3e053f50ad8
LEBOCQ Titouan <lebocq.titouan@gmail.com>
2021-10-13 10:05:01 +0200

    adding source in wall following algo

--- lib/mazeSolvingAlgorythms/wallFollower/wallFollower.h
+++ lib/mazeSolvingAlgorythms/wallFollower/wallFollower.h
@@ -1,6 +1,8 @@
 #ifndef WALL_FOLLOWER_H
 #define WALL_FOLLOWER_H
 
+// https://en.wikipedia.org/wiki/Maze-solving_algorithm#:~:text=be%20extremely%20slow.-,Wall%20follower,-%5Bedit%5D
+
 #include "../mazeSolvingAlgorythm.h"
 
 #endif //WALL_FOLLOWER_H
